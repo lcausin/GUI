93b6fc07990bf3c992e2bf3936ec76e4d75af96b
lcausin <test@test.com>
2019-03-05 15:25:45 +0100

    Builder files

---
+++ Cooking/LINUX/x86/src/Extension/CMakeFiles/Extension.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Extension.dir/Extension.cpp.o"
+  "/opt/shine_sdk_external/Bin/Extensions/libExtension.pdb"
+  "/opt/shine_sdk_external/Bin/Extensions/libExtension.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Extension.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
