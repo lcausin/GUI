01c2ae8f024f064d61da20b7e085f9f6d3039ac5
lcausin <test@test.com>
2019-03-05 12:12:01 +0100

    Builder files

---
+++ Cooking/LINUX/x86/src/Plugins/Plugin/CMakeFiles/Plugin_Editor.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/saas_git_clone/src/Plugins/Plugin/Plugin.cpp" "/tmp/saas_git_clone/Cooking/LINUX/x86/src/Plugins/Plugin/CMakeFiles/Plugin_Editor.dir/Plugin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SH_EDITOR=1"
+  "SH_MASTER=1"
+  "_INTERNAL_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/shine_sdk_external/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
