452e2c57806b8915c372b74dafcc79b5fb47c6ca
lcausin <laura.causin@laposte.net>
2019-03-18 18:03:19 +0100

    envoie tout

---
+++ src/Game/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <ShSDK/ShSDK.h>
+#include <ShEntryPoint/ShEntryPoint.h>
+
+#include <Plugin/Plugin.h>
+//
+// ShSDK
+#include "ShSDK/ShSDK.h"
+
+
+//
+// Callbacks declaration
+extern void	SetupDisplayProperties	(ShDisplayProperties & displayProperties);
+extern void OnPreInitialize			(void);
+extern void OnPostInitialize		(void);
+extern void OnPreUpdate				(float deltaTimeInMs);
+extern void OnPostUpdate			(float deltaTimeInMs);
+extern void OnPreRelease			(void);
+extern void OnPostRelease			(void);
+
+extern void	OnTouchDown				(int iTouch, float positionX, float positionY);
+extern void	OnTouchUp				(int iTouch, float positionX, float positionY);
+extern void	OnTouchMove				(int iTouch, float positionX, float positionY);
+
+extern void OnLogin					(ShUser * pUser);
+extern void OnLogout				(ShUser * pUser);
+extern void OnUserChanged			(ShUser * pUser);
