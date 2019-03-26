b4d21191e28398e45fe15e244a79a1b9efd8fb0b
lcausin <laura.causin@laposte.net>
2019-03-26 16:13:35 +0100

    all

--- Cooking/ANDROID/arm64-v8a/src/Game/CMakeFiles/BuildSAAS.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BuildSAAS"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BuildSAAS.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
